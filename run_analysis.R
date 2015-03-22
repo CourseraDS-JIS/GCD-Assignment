3c7b626bc8a5b2498f393c9bcdbf877c779e111e
julieinsan <julieinsignares@gmail.com>
2015-03-22 07:22:32 -0400

    first commit

---
+++ run_analysis.R
@@ -0,0 +1,41 @@
+library(dplyr)
+sub <- read.table("./test/subject_test.txt")
+x <- read.table("./test/X_test.txt")
+y <- read.table("./test/Y_test.txt")
+test <- cbind(sub,x,y, groupId = as.factor("TEST"))
+
+sub <- read.table("./train/subject_train.txt")
+x <- read.table("./train/X_train.txt")
+y <- read.table("./train/Y_train.txt")
+train <- cbind(sub,x,y, groupId = as.factor("TRAIN"))
+
+data <- rbind(test,train)
+
+n <- read.table("features.txt")
+n <- as.character(n[,2])
+n <- c("subject",n, "activity","groupID")
+n <-  make.names(names=n, unique=TRUE, allow_ = TRUE)
+colnames(data) <- n
+
+data$activities <- factor(data$activity, labels=c("WALKING", "WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING"))
+
+# refer to the documentation, make names makes mean() into mean..
+# I chose to not include means that had frequencies etc.
+data.analysis.mstd <- select(data, contains("mean..", ignore.case = TRUE),contains("std..", ignore.case = TRUE))
+data.analysis.mstd <- data.analysis.mstd[,order(names(data.analysis.mstd))]
+#data.analysis.std <- select(data, contains("std..", ignore.case = TRUE))
+
+new.data <- cbind(subject =data$subject, activity=data$activities, groupID=data$groupID, data.analysis.mstd)
+
+# Uses descriptive activity names to name the activities in the data set
+#new.data$activities <- factor(new.data$activity, labels=c("WALKING", "WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING"))
+new.data <- arrange(new.data, subject, activity)
+names(new.data) <- gsub("\\.\\.", "", names(new.data))
+
+# Appropriately labels the data set with descriptive variable names. 
+# You have to replace the ... and .. and somehow figure out how to do mean std
+# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+# Maybe something like this or aggregate function:
+data_d <- new.data %>% group_by(subject, activity) %>% summarise_each(funs(mean))
+## Data Citation: [1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
